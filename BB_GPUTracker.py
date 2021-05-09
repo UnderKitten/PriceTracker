f3de98db6d28514a2c4db3cd61ff0e184e84f865
UnderKitten <66450557+UnderKitten@users.noreply.github.com>
2021-05-09 15:04:59 -0400

    Cleaning/formatting

--- BB_GPUTracker.py
+++ BB_GPUTracker.py
@@ -13,6 +13,7 @@ product_list = ''
 load_dotenv()
 WEBHOOK = os.getenv('DISCORD_WEBHOOK')
 DIR = os.getenv('DIR')
+USER_AGENT=os.getenv('USER_AGENT')
 os.chdir(DIR)
 
 
@@ -24,34 +25,37 @@ def main():
     product_list = load_products()
 
     time.sleep(2)
-    main_menu()
+    access_url()
 
-
-def main_menu():
+def access_url():
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"}
+        "User-Agent": USER_AGENT}
 
-    # Let's access URL
-    for i in range(5):
+    # Let's access URLs
+    while True:
         for x in range(len(product_list)):
             process_page(product_list[x], headers)
         time.sleep(10)
         print('\n\n')
 
-
 def process_page(url, headers):
+    #Process HTML page
     page = requests.get(url, headers=headers)
     bs = BeautifulSoup(page.content, 'html.parser')
 
+    #Get product's title
     product_title = bs.find_all(class_="productName_3nyxM")
     title_text = product_title[0].get_text()
-
+    
+    #Get product's price
     product_price = bs.find_all(class_="large_3aP7Z")
     price_text = product_price[0].get_text()
 
+    #Get product's availability message
     product_availability = bs.find_all(class_="availabilityMessage_ig-s5")
     product_availability_text = product_availability[0].get_text().lower()
 
+    #Check if a product is available or out of stock
     if product_availability_text == "available to ship":
         status = "Available to Ship"
         discord_post(title_text, url)
@@ -62,12 +66,12 @@ def process_page(url, headers):
 
     print(f'{title_text}           {price_text}     {status}')
 
-
+    #Post a message on Discord via Bot
 def discord_post(title, url):
     webhook = Webhook.from_url(WEBHOOK, adapter=RequestsWebhookAdapter())
     webhook.send(f"@everyone {title}\n{url}")
 
-
+    #load product list from the .txt file
 def load_products():
     doc = open('products.txt')
     products = doc.read()
